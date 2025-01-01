c2d2c17226443c7ba61f8038a66e227c043d1bec
Ethan Heckart <heckart@proton.me>
2025-01-01 16:19:28 -0600

    removed unnecessary packages, added new binds, trying to make windows setup easier

--- config/evil-config.el
+++ config/evil-config.el
@@ -4,6 +4,11 @@
 (evil-define-key 'normal 'global (kbd "SPC d") 'lsp-ui-peek-find-definitions)
 (evil-define-key 'normal 'global (kbd "SPC r") 'lsp-ui-peek-find-references)
 (evil-define-key 'normal 'global (kbd "SPC t") 'treemacs-display-current-project-exclusively)
-(evil-define-key 'normal 'global (kbd "SPC T") 'treemacs)
+(evil-define-key 'normal 'global (kbd "SPC f") 'find-file)
+(evil-define-key 'normal 'global (kbd "SPC b") 'switch-to-buffer)
+(evil-define-key 'normal 'global (kbd "SPC j") 'previous-buffer)
+(evil-define-key 'normal 'global (kbd "SPC k") 'next-buffer)
+
+
 
 (require 'treemacs-evil)
--- init.el
+++ init.el
@@ -9,8 +9,8 @@
 ; them from here before launching emacs
 (setq package-selected-packages '(lsp-mode lsp-ui yasnippet lsp-treemacs helm-lsp
 					   projectile hydra flycheck company avy which-key helm-xref dap-mode ccls
-					   doom-modeline evil flycheck-rust lsp-pyright modus-themes
-					   tree-sitter tree-sitter-langs gcmh))
+					   doom-modeline evil flycheck-rust lsp-pyright tree-sitter tree-sitter-langs
+					   gcmh gruber-darker-theme treemacs-evil))
 
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
   (package-refresh-contents)
@@ -29,7 +29,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd" "8d146df8bd640320d5ca94d2913392bc6f763d5bc2bb47bed8e14975017eea91" "e410458d3e769c33e0865971deb6e8422457fad02bf51f7862fa180ccc42c032" default))
+   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd" default))
  '(global-word-wrap-whitespace-mode t)
  '(inhibit-startup-screen t)
  '(package-selected-packages
